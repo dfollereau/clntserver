c7521a1ac2b45cf0addaacfb2675c7853440342e
Denis FOLLEREAU <denis.follereau06@gmail.com>
2016-12-06 22:40:18 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 #sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials dfollereau-cluster
-sudo /opt/google-cloud-sdk/bin/gcloud docker login -e dfollereau@amadeus.com -u _token -p "$(gcloud auth print-access-token)" https://gcr.io
+sudo /opt/google-cloud-sdk/bin/gcloud docker login -e dfollereau@amadeus.com -u _token -p "$(/opt/google-cloud-sdk/bin/gcloud auth print-access-token)" https://gcr.io
 sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/dfollereau-cppserver:$CIRCLE_SHA1
 #sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 #kubectl patch deployment dfollereau-docker-cpp -p '{"spec":{"template":{"spec":{"containers":[{"name":"dfollereau-docker-cpp","image":"us.gcr.io/acs-dev/dfollereau-cppserver:'"$CIRCLE_SHA1"'"}]}}}}'
@@ -15,4 +15,4 @@ sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/dfol
 sudo /opt/google-cloud-sdk/bin/gcloud docker ps -a
 sudo /opt/google-cloud-sdk/bin/gcloud docker run --detach --publish 8080:8080 --name mygccapp gcr.io/${PROJECT_NAME}/dfollereau-cppserver:$CIRCLE_SHA1
 ls -al
-./client
+./client remote
