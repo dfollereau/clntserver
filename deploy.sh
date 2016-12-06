0d72ddb96fac9c41f9c569f6c7f23540d978cd7e
Denis FOLLEREAU <denis.follereau06@gmail.com>
2016-12-06 22:32:18 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@
 # Exit on any error
 set -e
 
-sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials dfollereau-cluster
-sudo /opt/google-cloud-sdk/bin/gcloud docker push eu.gcr.io/${PROJECT_NAME}/dfollereau-cppserver
+#sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials dfollereau-cluster
+sudo /opt/google-cloud-sdk/bin/gcloud docker login -e dfollereau@amadeus.com -u _token -p "$(gcloud auth print-access-token)" https://gcr.io
+sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/dfollereau-cppserver:$CIRCLE_SHA1
 #sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 #kubectl patch deployment dfollereau-docker-cpp -p '{"spec":{"template":{"spec":{"containers":[{"name":"dfollereau-docker-cpp","image":"us.gcr.io/acs-dev/dfollereau-cppserver:'"$CIRCLE_SHA1"'"}]}}}}'
 
@@ -12,6 +13,6 @@ sudo /opt/google-cloud-sdk/bin/gcloud docker push eu.gcr.io/${PROJECT_NAME}/dfol
 #kubectl expose deployment dfollereau-node --type="LoadBalancer"
 #kubectl get service dfollereau-node
 sudo /opt/google-cloud-sdk/bin/gcloud docker ps -a
-sudo /opt/google-cloud-sdk/bin/gcloud docker run --detach --publish 8080:8080 --name mygccapp gcr.io/${PROJECT_NAME}/dfollereau-cppserver
+sudo /opt/google-cloud-sdk/bin/gcloud docker run --detach --publish 8080:8080 --name mygccapp gcr.io/${PROJECT_NAME}/dfollereau-cppserver:$CIRCLE_SHA1
 ls -al
 ./client
