aba576d018a7a1cb4b4963437a5c06d390f1c461
Denis FOLLEREAU <denis.follereau06@gmail.com>
2016-12-06 22:14:48 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,14 @@
 set -e
 
 sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials dfollereau-cluster
-sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/dfollereau-cppserver
+sudo /opt/google-cloud-sdk/bin/gcloud docker push eu.gcr.io/${PROJECT_NAME}/dfollereau-cppserver
 #sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 #kubectl patch deployment dfollereau-docker-cpp -p '{"spec":{"template":{"spec":{"containers":[{"name":"dfollereau-docker-cpp","image":"us.gcr.io/acs-dev/dfollereau-cppserver:'"$CIRCLE_SHA1"'"}]}}}}'
-kubectl run dfollereau-node --us.gcr.io/${PROJECT_NAME}/dfollereau-cppserver:$CIRCLE_SHA1 --port=8080
-kubectl expose deployment dfollereau-node --type="LoadBalancer"
-kubectl get service dfollereau-node
+
+#kubectl run dfollereau-node --us.gcr.io/${PROJECT_NAME}/dfollereau-cppserver:$CIRCLE_SHA1 --port=8080
+#kubectl expose deployment dfollereau-node --type="LoadBalancer"
+#kubectl get service dfollereau-node
+sudo /opt/google-cloud-sdk/bin/gcloud docker ps -a
+sudo /opt/google-cloud-sdk/bin/gcloud docker run --detach --publish 8080:8080 --name mygccapp gcr.io/${PROJECT_NAME}/dfollereau-cppserver
+ls -al
+./client
